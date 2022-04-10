63e808c7448a24a7062af44b1378016c8bf9e97d
Diego Vieira <vieiracdiego@gmail.com>
2022-04-10 18:34:17 -0300

    Add a service to ChatBot API

---
+++ app/Services/ChatbotService.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Services;
+
+use GuzzleHttp\Client as GuzzleClient;
+use App\Services\AuthenticationService;
+
+class ChatbotService
+{
+    protected $apiKey = 'nyUl7wzXoKtgoHnd2fB0uRrAv0dDyLC+b4Y6xngpJDY=';
+    protected $baseEndpoint = 'https://api-gce3.inbenta.io/prod/chatbot/v1';
+
+    public function getAnswer($message)
+    {
+        $authService = new AuthenticationService();
+        $guzzleClient = new GuzzleClient();
+
+        $headers = [
+            'x-inbenta-key' => $this->apiKey,
+            'Authorization' => 'Bearer ' . $authService->getAuthToken(),
+            'x-inbenta-session' => 'Bearer ' . $authService->getConversationToken(),
+            'Content-Type' => 'application/json'
+        ];
+        $body = [
+            'message' => $message
+        ];
+
+        $response = $guzzleClient->request('POST', $this->baseEndpoint . '/conversation/message', [
+            'headers' => $headers,
+            'body' => json_encode($body)
+        ]);
+
+        return $response;
+    }
+}
