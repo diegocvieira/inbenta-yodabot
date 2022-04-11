789f5d4e336fb9c9290dedbe4aa267f82912e9e1
Diego Vieira <vieiracdiego@gmail.com>
2022-04-11 14:36:39 -0300

    Change Home name to Chat

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+use App\Http\Controllers\ChatController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +14,4 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index'])->name('chat');
+Route::get('/', [ChatController::class, 'index'])->name('chat');
